8f4448956e5f3aa2666ff61b9ba76398883e6bba
Cesar Martinez <cesarosvaldo.martinez@bbva.com>
2020-12-23 00:31:01 -0600

    Primera version

--- dile-social-icon.js
+++ dile-social-icon.js
@@ -1,9 +1,58 @@
 import { LitElement, html, css } from 'lit-element';
+import { icons } from './icons'
 
-export class SocialIcon extends LitElement {
+/**
+ * 
+ * 
+ * 
+ * # <social-icon>
+ *  Web component to create social icons, based on Lit Element
+ *  the "icon" property is used to set  the social network to display
+ */
+export class DileSocialIcon extends LitElement {
     
-    render(){
-        return html``;
+    static get properties() {
+        return {
+            icon: {
+                type: String
+            }
+        }
     }
+
+    constructor() {
+        super();
+        this.icon = 'facebook'
+    }
+
+    static get styles() {
+        return css`
+         :host[hidden] { display: none;}
+         :host[hidden] { display: inline-block;}
+         path {
+             fill: var(--dile-social-icon-color, #888);
+         }
+         path[fill="none"] {
+             fill: transparent;
+         }
+         svg {
+             width: var(--dile-social-icon-size, 24px);
+             height: var(--dile-social-icon-size, 24px);
+         }
+         div { display: flex;}
+        `
+    }
+
+    render(){
+        return html`
+         <div>
+             ${this._getIcon(this.icon)}
+         </div>
+        `;
+    }
+
+    _getIcon(icon) {
+        return icons[icon];
+    }
+
 }
-customElements.define('social-icon', SocialIcon);
+customElements.define('dile-social-icon', DileSocialIcon);
